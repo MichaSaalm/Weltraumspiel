24e6f8beee0700d7078279631ac1ab5de36dcf15
Micha <michasaalmueller@cosmoconsult.com>
2024-06-04 14:41:30 +0200

    building

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ app.use(express.static(path.join(__dirname, 'src')));
 app.get('/index', (req, res) => {
     res.sendFile(path.join(__dirname, 'src', 'index.html'));
 });
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
